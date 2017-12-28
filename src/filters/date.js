b12091a69b7444ab9e7c6459b0dc8ccdaae93b9c
plaski <maciejploskonka@gmail.com>
2017-12-28 23:35:44 +0100

    date filter fixed

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,4 +1,12 @@
 export default (value) => {
   const date = new Date(value)
+  const gameDay = date.getDay()
+  const now = new Date()
+  const today = now.getDay()
+  if (gameDay === today) {
+    return 'today, ' + date.toLocaleString('pl-PL', {month: 'numeric', day: 'numeric', year: 'numeric'})
+  } else if (gameDay - today === 1) {
+    return 'tomorrow, ' + date.toLocaleString('pl-PL', {month: 'numeric', day: 'numeric', year: 'numeric'})
+  }
   return date.toLocaleString('pl-PL', {weekday: 'long', month: 'numeric', day: 'numeric', year: 'numeric'})
 }
